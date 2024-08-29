2d0671450943d437eca7aca14a3b3511e6f7e90a
Chaves Nicolas <105256291+NicolasChavesMorelli@users.noreply.github.com>
2024-08-29 10:58:29 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,114 @@
+export const perguntas = [
+    {
+        enunciado: "1. Qual é o seu gênero de jogo favorito?",
+        alternativas: [
+            {
+                texto: "A) Jogos de Estratégia",
+                afirmacao: [
+                    "O Estrategista. Você é alguém que valoriza o planejamento e a análise. Gosta de jogos que desafiem sua mente e permitem tomar decisões táticas. Sua personalidade é focada e detalhista, buscando sempre a melhor forma de alcançar seus objetivos."
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "B) Jogos de Ação",
+                afirmacao: [
+                    "O Aventureiro. Você adora ação e emoção. Prefere jogos que oferecem adrenalina e desafios rápidos. Sua personalidade é ousada e enérgica, sempre em busca de novas experiências e aventuras."
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "2. Como você se sente sobre cooperar com outros jogadores?",
+        alternativas: [
+            {
+                texto: "Adoro trabalhar em equipe",
+                afirmacao: [
+                    "O Colaborativo. Você valoriza o trabalho em equipe e gosta de colaborar para alcançar objetivos comuns. Sua personalidade é sociável e você se destaca em jogos que exigem coordenação e cooperação."
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Prefiro jogar sozinho",
+                afirmacao: [
+                    "O Independente. Você gosta de enfrentar desafios por conta própria e valoriza a liberdade em seus jogos. Sua personalidade é autossuficiente e você prefere jogos onde pode seguir seu próprio ritmo e estratégias."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "3. Qual é a sua abordagem em jogos de multiplayer online?",
+        alternativas: [
+            {
+                texto: "Sempre sigo uma estratégia definida",
+                afirmacao: [
+                    "O Estrategista. Você é alguém que gosta de planejar e executar estratégias meticulosas. Em jogos multiplayer, você busca a vitória através de táticas bem elaboradas e coordenação com sua equipe."
+                ],
+                proxima: 4,
+            },
+            {
+                texto: "Gosto de improvisar e adaptar-me ao momento",
+                afirmacao: [
+                    "O Adaptável. Você é flexível e gosta de ajustar suas táticas conforme o jogo evolui. Sua personalidade é ágil e você se destaca em ambientes dinâmicos onde a capacidade de improvisar é crucial."
+                ],
+                proxima: 5,
+            },
+        ]
+    },
+    {
+        enunciado: "4. Como você prefere enfrentar desafios em jogos?",
+        alternativas: [
+            {
+                texto: "Com um plano detalhado",
+                afirmacao: [
+                    "O Planejador. Você é detalhista e gosta de abordar os desafios com um plano bem estruturado. Sua personalidade é analítica, e você acredita que a preparação e a estratégia são chave para o sucesso."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "Com uma abordagem espontânea",
+                afirmacao: [
+                    "O Espontâneo. Você prefere enfrentar desafios com uma abordagem mais flexível e intuitiva. Sua personalidade é criativa e você gosta de encontrar soluções no momento, aproveitando a imprevisibilidade dos jogos."
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "5. Qual tipo de narrativa você prefere em um jogo?",
+        alternativas: [
+            {
+                texto: "Histórias profundas e complexas",
+                afirmacao: [
+                    "O Narrativo. Você valoriza uma boa história e gosta de jogos que oferecem enredos profundos e envolventes. Sua personalidade é imersiva e você se encanta com tramas bem desenvolvidas e personagens profundos."
+                ],
+                proxima: 6,
+            },
+            {
+                texto: "Ação intensa e gameplay focado",
+                afirmacao: [
+                    "O Apreciador de Ação. Você prefere jogos com ritmo rápido e ação constante. Sua personalidade é enérgica e você se diverte mais com jogos que priorizam a jogabilidade e a emoção em vez da narrativa complexa."
+                ],
+                proxima: 7,
+            },
+        ]
+    },
+    {
+        enunciado: "6. Como você escolhe seus jogos?",
+        alternativas: [
+            {
+                texto: "Baseado em recomendações e análises",
+                afirmacao: [
+                    "O Criterioso. Você pesquisa e analisa as recomendações e análises antes de escolher um jogo. Sua personalidade é detalhista e você prefere fazer escolhas informadas baseadas em avaliações e feedback."
+                ],
+            },
+            {
+                texto: "Pela popularidade e tendências",
+                afirmacao: [
+                    "O Trendsetter. Você gosta de estar atualizado com as últimas tendências e popularidade. Sua personalidade é sociável e você se empolga com jogos que estão em alta e são amplamente discutidos."
+                ],
+            },
+        ]
+    },
+];
